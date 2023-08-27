ff4bdde3c07520215d2645d5a5af37544c748619
Budro Bundy <budro.bundy69@gmail.com>
2023-08-28 00:03:37 +0200

    fix dlt-comment in notebook dlt-wikipedia-python.py

--- src/notebooks/dlt-wikipedia-python.py
+++ src/notebooks/dlt-wikipedia-python.py
@@ -6,16 +6,12 @@ from pyspark.sql.functions import *
 json_path = "/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-uncompressed-json/2015_2_clickstream.json"
 
 # COMMAND ----------
-@dlt.table(
-  comment="The raw wikipedia clickstream dataset, ingested from /databricks-datasets."
-)
+@dlt.table("The raw wikipedia clickstream dataset, ingested from /databricks-datasets.")
 def clickstream_raw():
   return (spark.read.format("json").load(json_path))
 
 # COMMAND ----------
-@dlt.table(
-  comment="Wikipedia clickstream data cleaned and prepared for analysis."
-)
+@dlt.table("Wikipedia clickstream data cleaned and prepared for analysis.")
 @dlt.expect("valid_current_page_title", "current_page_title IS NOT NULL")
 @dlt.expect_or_fail("valid_count", "click_count > 0")
 def clickstream_prepared():
@@ -28,9 +24,7 @@ def clickstream_prepared():
   )
 
 # COMMAND ----------
-@dlt.table(
-  comment="A table containing the top pages linking to the Apache Spark page."
-)
+@dlt.table("A table containing the top pages linking to the Apache Spark page.")
 def top_spark_referrers():
   return (
     dlt.read("clickstream_prepared")
